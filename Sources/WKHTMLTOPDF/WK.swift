81290aca0241cd664bc663d7aa6ac252e66e2995
Mihael Isaev <isaev.mihael@gmail.com>
2018-08-18 01:04:34 +0400

    Allow to pass an array of pages to the WK.generate function, closes #2

--- Sources/WKHTMLTOPDF/WK.swift
+++ Sources/WKHTMLTOPDF/WK.swift
@@ -7,11 +7,7 @@ public typealias WK = WKHTMLTOPDF
 
 public class WKHTMLTOPDF {
     private var params: [GeneralParam] = []
-    #if os(Linux)
-    private var pathToBinary = "/usr/bin/wkhtmltopdf"
-    #else
     private var pathToBinary = "/usr/local/bin/wkhtmltopdf"
-    #endif
     private var tmpDir = "/tmp/wkhtmltopdf"
     
     public init (pathToBinary: String? = nil, tmpDir: String? = nil, _ params: GeneralParam...) {
@@ -25,39 +21,75 @@ public class WKHTMLTOPDF {
     }
     
     public func generate<Page>(container: Container, pages: Page...) throws -> EventLoopFuture<Data> where Page: PageProtocol {
+        return try generate(container: container, pages: pages)
+    }
+    
+    public func generate<Page>(container: Container, pages: [Page]) throws -> EventLoopFuture<Data> where Page: PageProtocol {
         let fm = FileManager()
         try fm.createDirectory(atPath: tmpDir, withIntermediateDirectories: true)
         
         var generatedPages: [GeneratedPage] = []
         return try pages.forEach(on: container, fm: fm, tmpDir: self.tmpDir) { page in
             generatedPages.append(page)
-        }.map {
-            defer {
-                for page in generatedPages {
-                    try? fm.removeItem(atPath: page.path)
+            }.flatMap {
+                defer {
+                    for page in generatedPages {
+                        try? fm.removeItem(atPath: page.path)
+                    }
                 }
-            }
-            let pageArgs = generatedPages.flatMap { page -> [String] in
-                var result = page.params.flatMap { $0.values }
-                result.append(page.path)
-                return result
-            }
-            let wk = Process()
-            let stdout = Pipe()
-            defer {
-                stdout.fileHandleForReading.closeFile()
-            }
-            wk.launchPath = self.pathToBinary
-            if !self.params.contains(.quiet()) {
-                self.params.append(.quiet())
-            }
-            wk.arguments = self.params.flatMap { $0.values }
-            wk.arguments?.append(contentsOf: pageArgs)
-            wk.arguments?.append("-") // output to stdout
-            wk.standardOutput = stdout
-            wk.launch()
-            return stdout.fileHandleForReading.readDataToEndOfFile()
+                let pageArgs = generatedPages.flatMap { page -> [String] in
+                    var result = page.params.flatMap { $0.values }
+                    result.append(page.path)
+                    return result
+                }
+                let wk = Process()
+                let stdout = Pipe()
+                wk.launchPath = self.pathToBinary
+                if !self.params.contains(.quiet) {
+                    self.params.append(.quiet)
+                }
+                let bars = [try self.leafHeader(on: container, fm: fm, tmpDir: self.tmpDir),
+                            try self.leafFooter(on: container, fm: fm, tmpDir: self.tmpDir)]
+                return bars.flatten(on: container).map {
+                    wk.arguments = self.params.flatMap { $0.values }
+                    wk.arguments?.append(contentsOf: pageArgs)
+                    wk.arguments?.append("-") // output to stdout
+                    wk.standardOutput = stdout
+                    wk.launch()
+                    return stdout.fileHandleForReading.readDataToEndOfFile()
+                }
         }
     }
     
+    public func set(_ p: GeneralParam) {
+        params.append(p)
+    }
+}
+
+//MARK: Process Leaf Header and Footer
+
+extension WKHTMLTOPDF {
+    private func leafHeader(on container: Container, fm: FileManager, tmpDir: String) throws -> Future<Void> {
+        if let option = self.params.first(where: { $0.isHeaderLeaf }), let pageBar = option.pageBar {
+            if let index = self.params.index(where: { $0.isHeaderHtml }) {
+                self.params.remove(at: index)
+            }
+            return try pageBar.generate(container, fm: fm, tmpDir: tmpDir).map { v in
+                self.params.append(.headerHtml(url: v.path))
+            }
+        }
+        return container.eventLoop.newSucceededFuture(result: ())
+    }
+    
+    private func leafFooter(on container: Container, fm: FileManager, tmpDir: String) throws -> Future<Void> {
+        if let option = self.params.first(where: { $0.isFooterLeaf }), let pageBar = option.pageBar {
+            if let index = self.params.index(where: { $0.isFooterHtml }) {
+                self.params.remove(at: index)
+            }
+            return try pageBar.generate(container, fm: fm, tmpDir: tmpDir).map { v in
+                self.params.append(.footerHtml(url: v.path))
+            }
+        }
+        return container.eventLoop.newSucceededFuture(result: ())
+    }
 }
